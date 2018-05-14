e4d117ab0395a6d2df46b3fc4a4bddc69f6a29e2
Jacqueline Buchmaier <you@example.com>
2018-05-14 15:39:47 +0200

    Firebase integration and first push notification

---
+++ app/src/main/java/com/buchmaier/jacqueline/mamiwata/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package com.buchmaier.jacqueline.mamiwata;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import android.util.Log;
+
+/**
+ * Created by jacqueline on 14.05.18.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
