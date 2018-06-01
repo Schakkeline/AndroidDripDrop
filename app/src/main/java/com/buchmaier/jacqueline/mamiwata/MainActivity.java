468f5bad9530f86fd18cbc7505d04142e9ae028e
Jacqueline Buchmaier <you@example.com>
2018-06-01 16:41:01 +0200

    setting up settings-fragment

--- app/src/main/java/com/buchmaier/jacqueline/mamiwata/MainActivity.java
+++ app/src/main/java/com/buchmaier/jacqueline/mamiwata/MainActivity.java
@@ -12,11 +12,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Text Dummy - TODO: Delete after testing
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -24,17 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     HomeFragment selectedFragment1 = new HomeFragment();
                     transaction.replace(R.id.content, selectedFragment1);
                     break;
                 case R.id.navigation_donations:
-                    mTextMessage.setText(R.string.title_donations);
                     DonationsFragment selectedFragment2 = new DonationsFragment();
                     transaction.replace(R.id.content, selectedFragment2);
                     break;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
                     SettingsFragment selectedFragment3 = new SettingsFragment();
                     transaction.replace(R.id.content, selectedFragment3);
                     break;
@@ -61,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.content, selectedFragment1);
         transaction.commit();
 
-        // Test-Text - TODO: Delete after testing
-        mTextMessage = findViewById(R.id.message);
     }
 
 }
--- app/src/main/java/com/buchmaier/jacqueline/mamiwata/SettingsFragment.java
+++ app/src/main/java/com/buchmaier/jacqueline/mamiwata/SettingsFragment.java
@@ -4,12 +4,26 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+
+    private SeekBar seekBar;
+    private TextView textView;
+    public EditText weight;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -18,8 +32,68 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Spinner Gender
+        String [] valuesGender = {"Female","Male",};
+        Spinner spinnerGender = v.findViewById(R.id.genderUser);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_spinner_item, valuesGender);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerGender.setAdapter(adapter);
+        spinnerGender.setOnItemSelectedListener(this);
+
+        // EditText Weight
+        weight   = v.findViewById(R.id.weightUser);
+        String myweight = weight.getText().toString();
+        Log.d("EditText Weight", "Value: " + myweight);
+
+        // Switch Sport
+        Switch sport = v.findViewById(R.id.sportUser);
+        Log.d("Switch Sport", "Value: " + sport.isChecked());
+
+        // SeekBar Water
+        seekBar = v.findViewById(R.id.seekBarWater);
+        textView = v.findViewById(R.id.textSeekbar);
+        textView.setText(seekBar.getProgress() + " ml");
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                textView.setText(seekBar.getProgress() + " ml");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText(seekBar.getProgress() + " ml");
+            }
+        });
+
+        // Switch Notifications
+        Switch notifications = v.findViewById(R.id.notification_switch);
+        Log.d("Switch Notifications", "Value: " + notifications.isChecked());
+
+
+        return v;
+    }
+
+    // Gender Selected
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        Object gender = parent.getItemAtPosition(pos);
+        Log.d("ADebugTag", "Value: " + gender);
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
 
