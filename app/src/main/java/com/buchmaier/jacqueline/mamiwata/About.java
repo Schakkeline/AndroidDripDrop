dcb3637d96475a05b60ee407719be962e78971b6
Jacqueline Buchmaier <you@example.com>
2018-06-26 13:15:40 +0200

    added Website link to about page

--- app/src/main/java/com/buchmaier/jacqueline/mamiwata/About.java
+++ app/src/main/java/com/buchmaier/jacqueline/mamiwata/About.java
@@ -1,7 +1,11 @@
 package com.buchmaier.jacqueline.mamiwata;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        RelativeLayout websiteLink = findViewById(R.id.website_clickable);
+        websiteLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://mamiwata-fe798.firebaseapp.com/"));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 }
